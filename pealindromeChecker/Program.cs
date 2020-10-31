bf9306742abeebb1d127b139cbc9f02f436de11d
Tayyip Doğu AYDIN <tdaydin@gmail.com>
2020-10-31 17:28:31 +0300

    Add project files.

---
+++ pealindromeChecker/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pealindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** This program checks if a word is palindrome or not ***");
+
+            string input = "";
+
+            while (input != "q")
+            {
+                Console.Write("Please enter a word: ");
+                input = Console.ReadLine();
+                string output = Reverse(input);
+                //Console.WriteLine(output);
+
+                if (output == input)
+                {
+                    Console.WriteLine(input + " is a palindrome word" + "\n");
+                }
+                else
+                {
+                    Console.WriteLine(input + " is not a palindrome word" + "\n");
+                }
+
+                Console.WriteLine("To exit the program, please enter \"q\"");
+            }
+
+            //Console.ReadLine();
+        }
+
+        static string Reverse(string input)
+        {
+            string result = "";
+            for (int i = (input.Length - 1); i >= 0; i--)
+            {
+                result = result + input[i];
+            }
+
+            return result;
+        }
+    }
+}
